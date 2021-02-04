552ada0fd5822c959c58a7226cf2ae486daa0d40
Natallia Safronenko <Natallia_Safronenko@epam.com>
2021-02-04 09:22:32 +0300

    Addinf the 3rd task

--- Attempt3.py
+++ Attempt3.py
@@ -1,1 +1,16 @@
-print("Go to sleep")
+
+# paragraphs = "  dqwqwQWqwQWEQWEq TweQWEQWT Q33Q weqwe  "
+# for paragraph in paragraphs:
+#     paragraph = paragraph.lower()
+#     print(paragraph)
+
+#
+# additional_sentance = 'Dfffsfsf sdfsddd sadda. Essfsfd sdsd.'
+# additional_sentance += words[len(words) - 1] + " "
+# print(additional_sentance)
+
+
+paragraph = "Ffff gggg dddd Ssfvsfsdf fffff."
+sentences = paragraph.split(".")        # Splitting paragraph into a sentences using dot as a separator
+sentences.remove('')
+print(sentences)
